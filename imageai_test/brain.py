3699fb83ed6f9f23d58f17ca4af8b6b64c4708c4
Chan Chun Kiu <107382038+chanchunkiu@users.noreply.github.com>
2023-08-23 21:29:23 +0900

    Add files via upload

---
+++ imageai_test/brain.py
@@ -0,0 +1,14 @@
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd() #current working direcotry
+prediction = ImageClassification()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2-b0353104.pth"))
+prediction.loadModel()
+
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "godzilla.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
+
